26e9746ca64a53423c432601666cf2cf016b9e76
Дмитрий Чернявский <cherndiman@gmail.com>
2013-10-12 15:12:08 +0400

    Исправлены id элементов для тестировщиков.

--- application/views/templates/authview.php
+++ application/views/templates/authview.php
@@ -1,19 +1,19 @@
 <?php echo HTML::style('media/css/authview.css');?>
 
-<form class="Login" action="" method="post">
+<form class="Login"  action="" method="post">
 <div class="FormTopBorder">Введите ваши имя пользователя и пароль</div>
 <div class="FormArea">
 	Имя пользователя: <br>
-    <input type="text" class="TextBox" /> <br>
+    <input type="text" class="TextBox" id="login" /> <br>
     <p></p>
     Пароль: <br>
-    <input type="password" class="TextBox" /> <br>
+    <input type="password" class="TextBox" id="password" /> <br>
     <?php if(isset($error)){?>
     	<p></p>
-    	<span id="fail">Имя пользователя или пароль введены неверно!</span>
+    	<span class="fail">Имя пользователя или пароль введены неверно!</span>
     <?}?>
 </div>
 <div class="FormBottomBorder">
-	<input type="submit" class="EntBut EntBut-color" id="EntAuth" value="Войти" name="subm" />
+	<input type="submit" class="EntBut EntBut-color EntAuth" id="submit" value="Войти" name="subm" />
 </div>				
 </form>
