1f5f753048e86e227af3a74ac84ef6e32aa94b83
Babur <btabys2010@gmail.com>
2013-10-12 15:50:29 +0400

    Диман Чернявский когда свои дела делаешь: нетрогай атрибут "name" там в полях <input type="text">
    А то ты стер вот и залогинится не могли)

--- application/views/templates/authview.php
+++ application/views/templates/authview.php
@@ -4,10 +4,10 @@
 <div class="FormTopBorder">Введите ваши имя пользователя и пароль</div>
 <div class="FormArea">
 	Имя пользователя: <br>
-    <input type="text" class="TextBox" id="login" /> <br>
+    <input type="text" class="TextBox" id="login" name="login" /> <br>
     <p></p>
     Пароль: <br>
-    <input type="password" class="TextBox" id="password" /> <br>
+    <input type="password" class="TextBox" id="password" name="password"/> <br>
     <?php if(isset($error)){?>
     	<p></p>
     	<span class="fail">Имя пользователя или пароль введены неверно!</span>
--- application/views/templates/log_buton.php
+++ application/views/templates/log_buton.php
@@ -1,3 +1,3 @@
 <form action="<?php echo URL::site('auth/') ?>" method="post">
-<input type="submit"  id="submit" value="Войти" style="width:55px; height:30px" name="submmit">
+<input type="submit"  id="login" value="Войти" style="width:55px; height:30px" name="submmit">
 </form>
