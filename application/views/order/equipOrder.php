28c62b71e69f3c6f71c892784421c639a58b799c
IlyaPetrov <pet1991rik@gmail.com>
2013-10-18 22:19:15 +0400

    Добавил view укомплектования заказа

---
+++ application/views/order/equipOrder.php
@@ -0,0 +1,8 @@
+﻿<form> 
+<h1 align = "center"> На данный момент комплектуются заказы для интервала <br> 
+<input type = "text" name = "Time" Value = "12:00 - 12:15"> </h1> <br>
+<label>Всего заказов на интервал:<input type = "text" name = "totalOrder" Value = "35" size = 5></label> 
+<div align = "right"> <label>Осталось укомплектовать:<input type = "text" name = "balance" value = "3" size = 5></label></div><br>
+<div align = "center"><input type = "submit" value = "Получить заказ для комплектования"></div><br>
+<div align = "center"><input type = "submit" value = "Просмотреть заказ"></div>
+</form>
