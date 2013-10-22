c4fcfa8a0972f47a8cfd9ee24176bba8980a17d1
SleepingCat <iskander7952@yandex.ru>
2013-10-23 02:17:11 +0400

    обновил вьюшку согласно ВИ

--- application/views/order/order.php
+++ application/views/order/order.php
@@ -1,18 +1,24 @@
+<style>
+td,table 
+{
+	border:1px solid black;
+}
+</style>
 <div>
 	Ваши заказы:<br>
 <table>
+<tr><td>Номер заказа</td><td>Дата заказа</td><td>Время заказа</td><td>Статус заказа</td></tr>
 	<?php
 /**
  * Вывод ВСЕХ заказов пользователя без возможности редактирования 
  * с возможностью отменить заказ, если тот не в комплектовании
  */
-
 	$summ = 0;
 	foreach ($orders as $key => $value)
 	{
-		echo "<tr><td>".$value['order_id']."</td><td>".$value['order_date']."</td>
-				<td>".$value['order_date']."</td>
-			<td><a href=\"http://".$_SERVER['HTTP_HOST']."/order/cancel/".$value['order_id']."\">Отменить</a></td><td><a href=#>Изменить</a></td></tr>";
+		echo "<tr><td>".$value['order_id']."</td><td>".$value['delivery_date']."</td>
+			<td>".$value['delivery_time']."</td><td>".$value['order_status']."</td>
+			<td><a href=\"http://".$_SERVER['HTTP_HOST']."/order/cancel/".$value['order_id']."\"><button>Отменить</button></a></td><td><a href=#><button>Изменить</button></a></td></tr>";
 	}
 ?>
 </table>
