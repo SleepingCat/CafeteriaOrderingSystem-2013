8998a3da9f9a1632671885ecbfae58f158a413cc
Соколов <sokolov2211@gmail.com>
2013-10-23 09:06:43 +0400

    Вставил в модель свои запросы

--- application/classes/Model/Order.php
+++ application/classes/Model/Order.php
@@ -70,4 +70,28 @@ class Model_Order extends Model
 			}
 		}
 	}
+	
+	/**
+	 * Находит заказ по номеру возвращает его текущий статус
+	 * @param unknown $ID номер заказ
+	 * @return Ambigous <$this, Database_Query>
+	 */
+	public function findOrder($ID)
+	{
+		$OrderStatus = DB::query(Database::SELECT, 'select order_status from Orders where order_id = :ID')
+		-> param(':ID', $ID);
+		return $OrderStatus;
+	}
+	
+	/**
+	 * Устанавливает статус заказа
+	 * @param unknown $ID - номер заказа
+	 * @param unknown $UpdStatus - статус, который нужно установаить
+	 */
+	public function setStatus($ID,$UpdStatus)
+	{
+		DB::query(Database::UPDATE, 'update Orders set order_status = :St where order_id = :ID')
+		-> param(':ID', $ID)
+		-> param(':St', $UpdStatus);
+	}
 }
