ebba5e16f9e84b087887d07cd8f3d22743488f15
Дмитрий Чернявский <cherndiman@gmail.com>
2013-10-31 19:35:02 +0400

    Отображение меню навигации

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -11,7 +11,7 @@
 <body>	
 <div class="MainArea">
 <header>
-	<div class="Personal"> <?php echo $user; ?> </div>
+	<div class="Personal"> <?php echo $user; ?></div>
 	
 	<?php echo HTML::image('media/image/MainTemplate/Logo.png', array('class' => 'Logo')); ?>  
 </header>
@@ -21,34 +21,27 @@
     
 <!-- Кнопки главной панели -->
   <?php if( $guest == "Гость")    
-    {?>  
-    <a href="<?php echo URL::site() ?>" class="MainPanelButton"> <?php echo __('Гостевая страница') ?>
-    </a><a href="<?php echo URL::site() ?>" class="MainPanelButton LeftButton"> <?php echo __('Реклама') ?>
+    {?>    	
+     <a href="<?php echo URL::site() ?>" class="MainPanelButton LeftButton"> <?php echo __('Реклама') ?>
      </a><a href="<?php echo URL::site() ?>" class="MainPanelButton"> <?php echo __('Новости') ?>
-     </a><a href="<?php echo URL::site() ?>" class="MainPanelButton"> <?php echo __('Контактные данные') ?>
-     </a> 
-     
+     </a><a href="<?php echo URL::site() ?>" class="MainPanelButton"> <?php echo __('Контактные данные') ?></a> 
     <?}
     ?>
 
     
 <?php $ind = 0;
-//класс LeftButton должен применяться только к крайней левой кнопке,
-//возможно есть способ реализовать это получше, но оно пока работает
  foreach ($menu as $item) : 
  $ind += 1;?><a href="<?php echo URL::site($item['name'])?>" class="MainPanelButton
   	<?php if($ind == 1)
   		echo ' LeftButton';
-//класс ActiveButton должен применяться к кнопке, соответствующей открытой странице,
-//поскольку name содержит не весь адрес, то такой вариант не работает, надо придумать как сделать
-  	if($item['name'] == URL::site())
-		echo ' ActiveButton' ?>
+  	if($item['name'] == $_SERVER['REQUEST_URI'])
+		echo ' ActiveButton';?>
 	"><?php echo ($item['name_link'])?></a><?php endforeach; ?>    
 <!--  -->
-   
+
+    
 </div>
-<?php echo $message."<br>"; 
-   echo $content; ?>
+<?php echo $content; ?>
 </div>
  
 <footer class="Underground">Подвал</footer>
