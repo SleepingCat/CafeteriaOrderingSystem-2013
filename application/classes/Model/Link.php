943389fa7752787bc8cf2dfdf2bac1cecd7c6a4c
Babur <btabys2010@gmail.com>
2013-11-06 00:28:17 +0400

    Значит так: Вроде как дополнил модель Link, добавил туда чтобы вытаскивало ссылки на панель навигацию еще на остальные роли: такие как: менеджер меню,менеджер заказа,комплектовщик!
    Имеется на основе вовенного скрипта(DataBaseSheme revision 3.1with CheckRights.sql) ,скрипт заполенный данными из папки Данные для БД (все таблицы 4-й ревизии), а такеже добавил туда ссылки на экшены взависимости от роли!
    
    Если захотите запустить этот скриптик,авторизация настроена следующим образом:
    
    Каждому создал польз-ля:
    
    Т.е логин равняется вашей фамилии, например sokolov ,кроме Вакина Димы, у которого короткая фамилия),поэтому логин у него VakinD
    
    Пароль у всех 111111
    
    Сейчас заполняется лишь панель навигации взависимости от роли,другие экшены и контролы пока доступны всем пол-м кроме админской панели!
    
    Да у Сани Масалина роль клиент ,поэтому в этом крипте есть клиент UmnovDE пароль также 111111

--- application/classes/Model/Link.php
+++ application/classes/Model/Link.php
@@ -1,13 +1,13 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 /**
- * Модель ,содержит метод для вытаскивания ссылок панели навигаци
+ * Модель ,содержит метод для вытаскивания ссылок панели навигации
  * 
  */
 class Model_Link 
 {	
 	/**
-	 * Возвращает cсылку,и название сслылки
+	 * Возвращает cсылку,и название ссылки
 	 * 
 	 * @return $link
 	 */
@@ -25,21 +25,53 @@ class Model_Link
 				->param(':username', $username)
 				->param(':admin', 'admin')
 				->execute()
-				->as_array();		
+				->as_array();
+		}		
 		
+		
+		if ($auth->logged_in('login') && $auth->logged_in('manager_order'))
+		{		
+			return DB::query(Database::SELECT,
+					"select menu_buttons.name,menu_buttons.name_link from menu_buttons join roles_buttons on menu_buttons_id=menu_buttons.id join roles on roles.id=roles_buttons.roles_role_Id join roles_users on roles_users.role_id=roles_buttons.roles_role_Id
+		join users on roles_users.user_id=users.id where users.username=:username and roles.name=:admin order by  menu_buttons.name")
+				->param(':username', $username)
+				->param(':admin', 'manager_order')
+				->execute()
+				->as_array();		
+		}
+		
+		if ($auth->logged_in('login') && $auth->logged_in('complete'))
+		{
+			return DB::query(Database::SELECT,
+					"select menu_buttons.name,menu_buttons.name_link from menu_buttons join roles_buttons on menu_buttons_id=menu_buttons.id join roles on roles.id=roles_buttons.roles_role_Id join roles_users on roles_users.role_id=roles_buttons.roles_role_Id
+		join users on roles_users.user_id=users.id where users.username=:username and roles.name=:admin order by  menu_buttons.name")
+				->param(':username', $username)
+				->param(':admin', 'complete')
+				->execute()
+				->as_array();
+		}
+		
+		if ($auth->logged_in('login') && $auth->logged_in('manager_menu'))
+		{
+			return DB::query(Database::SELECT,
+					"select menu_buttons.name,menu_buttons.name_link from menu_buttons join roles_buttons on menu_buttons_id=menu_buttons.id join roles on roles.id=roles_buttons.roles_role_Id join roles_users on roles_users.role_id=roles_buttons.roles_role_Id
+		join users on roles_users.user_id=users.id where users.username=:username and roles.name=:admin order by  menu_buttons.name")
+				->param(':username', $username)
+				->param(':admin', 'manager_menu')
+				->execute()
+				->as_array();
 		}
 		
 		if ($auth->logged_in('login'))
 		{
 		
 			return DB::query(Database::SELECT,
-			"select menu_buttons.name,menu_buttons.name_link from menu_buttons join roles_buttons on menu_buttons_id=menu_buttons.id join roles on roles.id=roles_buttons.roles_role_Id join roles_users on roles_users.role_id=roles_buttons.roles_role_Id
+					"select menu_buttons.name,menu_buttons.name_link from menu_buttons join roles_buttons on menu_buttons_id=menu_buttons.id join roles on roles.id=roles_buttons.roles_role_Id join roles_users on roles_users.role_id=roles_buttons.roles_role_Id
 		join users on roles_users.user_id=users.id where users.username=:username and roles.name=:admin order by  menu_buttons.name")
 				->param(':username', $username)
 				->param(':admin', 'login')
 				->execute()
 				->as_array();
-		
 		}
 	
 		
