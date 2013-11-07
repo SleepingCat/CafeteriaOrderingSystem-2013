7f5bc0ab38ff99f0943c47156163109ea5677af4
Alex E. Khochenkov <Alexkhochenkov@gmail.com>
2013-11-07 22:07:14 +0300

    Доработал отображение реестра под последнюю версию БД. Внес изменение в добавление блюда. Внес изменение в удаление блюда согласно требованиям.

--- application/views/reestr/reestr.php
+++ application/views/reestr/reestr.php
@@ -1,24 +1,15 @@
+<!-- @autor=MrAnderson -->
+
 <div align="center">
-<!--  
-<style>
-td,table 
-{
-	margin-top: 35px;	
-	text-align: left;
-	border-collapse: collapse;
-	color: #F5D694;	
-	
-}
-</style>
--->
 
 <table class = "Datatable">
   <tr>
     <th class = "Datacell TabHeader">Название</th>
     <th class = "Datacell TabHeader">Ингридиенты</th>
-    <th class = "Datacell TabHeader">Порции</th>
+    <th class = "Datacell TabHeader">Статус</th>
     <th class = "Datacell TabHeader">Тип</th>
     <th class = "Datacell TabHeader">Категория</th>
+    <th class = "Datacell TabHeader">Является <br> стандартным</th>
     <th class = "Datacell TabHeader"> </th>
     <th class = "Datacell TabHeader"> </th>
   </tr>
@@ -28,21 +19,36 @@ foreach ($reestr as $key=>$value)
 {
 	echo "<tr>";
 	echo "<td  class = \"Datacell\">".$value['dish_name']."</td>"; 
+	
 	echo "<td  class = \"Datacell\">";
+
     foreach ($value['ingredients'] as $key1=>$value1)
 	{
 	 	echo $value1['product_name']."<br>";
 	}		
+	
 	echo "</td>";
-	echo "<td class = \"Datacell\">";
-	foreach ($value['portions'] as $key1=>$value1)
+	
+	if ($value['is_available'] == '1')
 	{
-		echo $value1['type_name']."<br>";
+	echo "<td class = \"Datacell\"> Доступно <br> для заказа</td>";
 	}
-	echo "</td>";
+	else 
+	{
+		echo "<td class = \"Datacell\"> Не доступно <br> для заказа </td>";
+	}
+		
 	echo "<td class = \"Datacell\">".$value['type']."</td>";
 	echo "<td class = \"Datacell\">".$value['category']."</td>";
 	
+	if($value['is_standart'] == 1)
+	{
+	echo "<td class = \"Datacell\"> Да </td>";
+	}
+	else 
+	{
+		echo "<td class = \"Datacell\"> Нет </td>";
+	}	
 	echo "<td class = \"Datacell\"><a onclick=\"return confirm('?')\"  href=\"http://".$_SERVER['HTTP_HOST']."/reestr/delete/".$value['dish_id']."\"><button>Удалить</button></a></td>
     	  <td class = \"Datacell\"><a href=\"http://".$_SERVER['HTTP_HOST']."/reestr/edit/".$value['dish_id']."\"><button>Изменить</button></a></td></tr>";
 	echo "</tr>";
