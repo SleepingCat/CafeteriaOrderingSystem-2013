0e7c650f23e902645ea6abe663943f9f1bc36d24
PetrovIlya <pet1991rik@gmail.com>
2013-11-13 00:57:24 +0400

    Небольшие доработки

--- application/views/ingridients/listIngr.php
+++ application/views/ingridients/listIngr.php
@@ -2,11 +2,13 @@
 	<div id = "добавление" align = "center"> <label for = "добавление"> Добавить новый ингредиент: </label>
 	 <input type = "submit" name = "newIngr" value = "Добавить"> </div> <br>
 			<div align = "center"> Список ингредиентов: </div>
-				<div align = "center"><table border = solid 1px black bgcolor = #A0522D> <tr> <td> Ингредиент </td><td> Количество </td></tr>
+				<div align = "center"><table border = solid 1px black bgcolor = #A0522D> <tr><td> √ </td> <td> Ингредиент </td><td> Количество </td></tr>
 					<?php
+						$i = 0;
 						foreach ($list as $key => $value)
 						{
-						   echo "<tr><td align = \"center\">".$value['product_name']."</td><td align = \"center\">".$value['balance']."</td></tr>";
+						   $i++;
+						   echo "<tr><td><input type=\"checkbox\" name=\"check".$i."\"></td><td align = \"center\">".$value['product_name']."</td><td align = \"center\">".$value['balance']."</td></tr>";
 						}
 					?>
 				</table></div><br>
