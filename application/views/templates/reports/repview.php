f2d833db78210a3ef4d82a3c3783f846d7ec9809
Babur <btabys2010@gmail.com>
2013-11-20 21:01:12 +0400

    Создал пока так прмиер вьюшек для отчетов!
    Да забыл надо создать стобец SeniorID в БД в тпблице menu)buttons ,это что касается предыдущего коммита!) А то не скзал како столбец добавлять)

---
+++ application/views/templates/reports/repview.php
@@ -0,0 +1,17 @@
+<form action="" method="post">
+
+	
+	
+	<div align = "center"><input type = "submit" name = "subm" style="width:150; height:30px"  value ="<?php echo __('Отчет по клиентам') ?>" /> </div>
+	
+	<div align = "center"><input type = "submit" name = "clientss" style="width:150; height:30px" value ="<?php echo __('Отчет по блюдам') ?>" /> </div>
+	
+	<div align = "center"><input type = "submit" name = "clientss" style="width:150; height:30px"  value ="<?php echo __('Отчет по заказам') ?>" /> </div>
+	
+	
+	
+	
+	
+					
+				
+</form>
