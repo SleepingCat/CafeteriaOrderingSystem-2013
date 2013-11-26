91673755123820a0ac5e37d5cdaeec96e68f5d90
PetrovIlya <pet1991rik@gmail.com>
2013-11-26 12:02:33 +0400

    Исправлено название таблицы

--- application/classes/Model/NewIngr.php
+++ application/classes/Model/NewIngr.php
@@ -4,7 +4,7 @@ class Model_NewIngr
 {
 	public function add_new_ingr($ingr, $balance)
 	{
-		$ingr = DB::query(Database::INSERT, 'Insert Into Products set product_name = :ingr, balance = :bal')
+		$ingr = DB::query(Database::INSERT, 'insert into products set product_name = :ingr, balance = :bal')
 		->param(':ingr', $ingr)
 		->param(':bal', $balance)
 		->execute();
