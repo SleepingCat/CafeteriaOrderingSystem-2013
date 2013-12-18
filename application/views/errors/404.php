dcc7afaf5c9d80a4621bde93d87b1f9e8e556f72
SleepingCat <iskander7952@yandex.ru>
2013-12-18 19:42:41 +0400

    404я страница

---
+++ application/views/errors/404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>404 Not Found</title>
+	<link rel="stylesheet" type="text/css" href="/media/css/reqsender.css">
+</head>
+<style>
+*{
+	color:#333;
+	font-family:Sans-Serif;
+	background: #eee;
+}
+H1{
+	Font-size: 50pt;
+}
+.content{
+	margin-top:13%;
+}
+</style>
+<body>
+	<div class=content>
+		<H1 align=center>404</H1>
+		<p align=center>
+		Not Found
+		</p>
+
+	</div>
+</body>
+</html>
